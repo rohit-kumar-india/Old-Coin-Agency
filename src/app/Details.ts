81b70c3111ffd150654a269de763efa70b81d066
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-06-08 17:29:46 +0530

    Updated emailid in details file

--- src/app/Details.ts
+++ src/app/Details.ts
@@ -2,7 +2,7 @@ export enum Details{
     title='Old Coin Agency',
     phone='+919631131636',
     whatsapp='919631131636',
-    email='rk2146.rohit@gmail.com@gmail.com',
+    email='rk2146.rohit@gmail.com',
     address='Behind Khetan Super Market, Birla Mandir Rd, Subjibagh, Bakarganj, Patna, Bihar 800004',
     formspree_url='https://formspree.io/f/mayzyook'
 }
